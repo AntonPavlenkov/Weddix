f3917b8f4d0d741517e8ec5dc99248ea9b86bba0
eladFrizi <okc.elad.35@gmail.com>
2017-07-05 18:12:00 +0300

    added color picker

---
+++ src/components/txtToolbar/ColorPicker.vue
@@ -0,0 +1,305 @@
+<template id="color-picker-template">
+    <div class="color-picker">
+        <div class="color-picker__overlay" v-if="isVisible" v-on:click="hide"></div>
+        <transition name="pop">
+            <div class="color-picker__flyout" v-if="isVisible">
+                <div class="color-chip" v-bind:style="{'background': color}">
+                    <div class="color-chip__inner">
+                        <h1 class="color-chip__title">HSL</h1>
+                        <h3 class="color-chip__subtitle">{{ colorString }}</h3>
+                    </div>
+                </div>
+                <div class="color-picker__inner">
+                    <div class="control" v-bind:style="gradientH">
+                        <input type="range" min="0" max="360" v-model="h" @input="pubilshColor" />
+                    </div>
+                    <div class="control" v-bind:style="gradientS">
+                        <input type="range" min="0" max="100" v-model="s" @input="pubilshColor" />
+                    </div>
+                    <div class="control" v-bind:style="gradientL">
+                        <input type="range" min="0" max="100" v-model="l" @input="pubilshColor" />
+                    </div>
+                </div>
+            </div>
+        </transition>
+        <div class="swatch" v-bind:style="{'background': color}" v-on:click="toggle"></div>
+    </div>
+</template>
+
+<script>
+function hsb2hsl(h, s, b) {
+    var hsl = {
+        h: h
+    };
+    hsl.l = (2 - s) * b;
+    hsl.s = s * b;
+
+    if (hsl.l <= 1 && hsl.l > 0) {
+        hsl.s /= hsl.l;
+    } else {
+        hsl.s /= 2 - hsl.l;
+    }
+
+    hsl.l /= 2;
+
+    if (hsl.s > 1) {
+        hsl.s = 1;
+    }
+
+    if (!hsl.s > 0) hsl.s = 0
+
+
+    hsl.h *= 360;
+    hsl.s *= 100;
+    hsl.l *= 100;
+
+    return hsl;
+}
+
+export default {
+    props: ["change", "initial"],
+    data: function () {
+        return {
+            isVisible: true,
+            h: 265,
+            s: 80,
+            l: 99
+        }
+    },
+    created() {
+        this.isVisible = false;
+
+    },
+    computed: {
+        color: function () {
+            var hsl = hsb2hsl(parseFloat(this.h) / 360, parseFloat(this.s) / 100, parseFloat(this.l) / 100)
+
+            var c = hsl.h + ", " + hsl.s + "%, " + hsl.l + "%";
+
+            var s = "hsl(" + c + ")";
+            this.change({
+                color: s
+            });
+            return s;
+        },
+        colorString: function () {
+            var c = this.h + ", " + this.s + "%, " + this.l + "%"
+            return c;
+        },
+        gradientH: function () {
+            var stops = [];
+            for (var i = 0; i < 7; i++) {
+                var h = i * 60;
+
+                var hsl = hsb2hsl(parseFloat(h / 360), parseFloat(this.s) / 100, parseFloat(this.l / 100))
+
+                var c = hsl.h + ", " + hsl.s + "%, " + hsl.l + "%"
+                stops.push("hsl(" + c + ")")
+            }
+
+            return {
+                backgroundImage: "linear-gradient(to right, " + stops.join(', ') + ")"
+            }
+        },
+        gradientS: function () {
+            var stops = [];
+            var c;
+            var hsl = hsb2hsl(parseFloat(this.h / 360), 0, parseFloat(this.l / 100))
+            c = hsl.h + ", " + hsl.s + "%, " + hsl.l + "%"
+            stops.push("hsl(" + c + ")")
+
+            var hsl = hsb2hsl(parseFloat(this.h / 360), 1, parseFloat(this.l / 100))
+            c = hsl.h + ", " + hsl.s + "%, " + hsl.l + "%"
+            stops.push("hsl(" + c + ")")
+
+            return {
+                backgroundImage: "linear-gradient(to right, " + stops.join(', ') + ")"
+            }
+        },
+
+        gradientL: function () {
+            var stops = [];
+            var c;
+
+            var hsl = hsb2hsl(parseFloat(this.h / 360), 0, 0)
+            c = hsl.h + ", " + hsl.s + "%, " + hsl.l + "%"
+            stops.push("hsl(" + c + ")")
+
+            var hsl = hsb2hsl(parseFloat(this.h / 360), parseFloat(this.s / 100), 1)
+
+            c = hsl.h + ", " + hsl.s + "%, " + hsl.l + "%"
+            stops.push("hsl(" + c + ")")
+
+            return {
+                backgroundImage: "linear-gradient(to right, " + stops.join(', ') + ")"
+
+            }
+        }
+    },
+    methods: {
+
+        show: function () {
+            this.isVisible = true;
+        },
+        hide: function () {
+            this.isVisible = false;
+        },
+        toggle: function () {
+            this.isVisible = !this.isVisible;
+        },
+        pubilshColor() {
+            this.$emit('changeColor', this.color)
+        }
+    },
+
+    mounted: function () {
+        this.h = parseInt(Math.random() * 360)
+    }
+}
+
+
+
+</script>
+
+<style>
+.color-picker {
+    position: relative;
+    line-height: 0px;
+}
+
+.color-picker__overlay {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0px;
+    left: 0;
+    background: black;
+    z-index: 0;
+    opacity: 0;
+}
+
+.color-picker__flyout {
+    width: 240px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    background: white;
+    box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.12);
+    font-family: "Roboto", "Helvetica Neue", sans-serif;
+    position: absolute;
+    bottom: -120px;
+    left: -100px;
+    z-index: 2;
+}
+
+.color-picker__inner {
+    padding: 1.5rem 1rem;
+}
+
+.color-chip {
+    height: 260px;
+    display: none;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    border-radius: 4px 4px 0 0;
+}
+
+.color-chip__inner {
+    text-align: center;
+}
+
+.color-chip__subtitle {
+    margin-top: 0.5rem;
+    opacity: 0.7;
+    font-weight: normal;
+    font-size: 15px;
+    text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.5);
+}
+
+.color-chip__title {
+    color: white;
+    margin: 0;
+    font-size: 50px;
+    letter-spacing: 4px;
+    font-family: Helvetica Neue;
+    text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.5);
+}
+
+.control {
+    width: 100%;
+    height: 12px;
+    border-radius: 12px;
+    border: 1px solid #ddd;
+}
+
+.control+.control {
+    margin-top: 1rem;
+}
+
+.control input {
+    width: 100%;
+    margin: 0;
+}
+
+.control input[type=range] {
+    -webkit-appearance: none;
+    width: 100%;
+    background: transparent;
+}
+
+.control input[type=range]:focus {
+    outline: none;
+}
+
+.control input[type=range]::-ms-track {
+    width: 100%;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+}
+
+.control input[type=range]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    border: 1px solid #ddd;
+    height: 20px;
+    width: 20px;
+    border-radius: 50px;
+    background: #ffffff;
+    cursor: pointer;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.12);
+    margin-top: -4px;
+}
+
+.swatch {
+    width: 24px;
+    height: 24px;
+    margin: 1rem auto 0 auto;
+    border: 4px solid white;
+    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+    width: 30px;
+    height: 33px;
+    margin: 0 0 0 5px;
+    opacity: 0;
+}
+
+.swatch:hover {
+    border: 4px solid white;
+    /*opacity: 0.8;*/
+    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.3);
+}
+
+.pop-enter-active,
+.pop-leave-active {
+    transition: transform .5s, opacity .5s;
+    transition-timing-function: cubic-bezier(.8, .3, 0.25, 1.75);
+    transform: scale(1);
+}
+
+.pop-enter,
+.pop-leave-active {
+    opacity: 0;
+    transform: scale(0);
+}
+</style>
--- src/components/txtToolbar/TxtToolbar.vue
+++ src/components/txtToolbar/TxtToolbar.vue
@@ -1,99 +1,97 @@
 <template>
-<section class="text-toolbar  md-mini">
-<md-button  class="md-fab md-clean  md-mini">
-    <md-icon>text_format</md-icon>
-     <md-tooltip md-direction="top">Choose text format</md-tooltip>
-  </md-button>
-
-  <md-button @click="changeAlign('right')" class="md-fab md-clean md-mini">
-    <md-icon>format_align_right</md-icon>
-     <md-tooltip md-direction="top">Text align right</md-tooltip>
-  </md-button>
-
-  <md-button @click="changeAlign('center')" class="md-fab md-clean  md-mini">
-    <md-icon>format_align_center</md-icon>
-     <md-tooltip md-direction="top">Text align center</md-tooltip>
-  </md-button>
-
-  <md-button @click="changeAlign('left')" class="md-fab md-clean  md-mini">
-    <md-icon>format_align_left</md-icon>
-     <md-tooltip md-direction="top">Text align left</md-tooltip>
-  </md-button>
-
-  <md-button @click="changeBold" class="md-fab md-clean  md-mini">
-    <md-icon>format_bold</md-icon>
-     <md-tooltip md-direction="top">Bold text</md-tooltip>
-  </md-button>
-
-<md-menu md-size="4">
-  <md-button md-menu-trigger class="md-fab md-clean  md-mini">
-    <md-icon>format_color_fill</md-icon>
-     <md-tooltip md-direction="top">Change color of the text</md-tooltip>
-  </md-button>
-  <md-menu-content>
-    <md-menu-item>
-      <span class="c1">White</span>
-    </md-menu-item>
-    <md-menu-item>
-      <span class="c2">Red</span>
-    </md-menu-item>
-    <md-menu-item>
-      <span class="c3">Green</span>
-    </md-menu-item>
-  </md-menu-content>
-</md-menu>
-
-
-<md-menu md-align-trigger>
-  <md-button md-menu-trigger class="md-fab md-clean  md-mini">
-    <md-icon>format_size</md-icon>
-     <md-tooltip md-direction="top">Change text size</md-tooltip>
-  </md-button>
-  <md-menu-content>
-    <md-menu-item>12px</md-menu-item>
-    <md-menu-item>16px</md-menu-item>
-    <md-menu-item>18px</md-menu-item>
-    <md-menu-item>20px</md-menu-item>
-    <md-menu-item>22px</md-menu-item>
-    <md-menu-item>24px</md-menu-item>
-  </md-menu-content>
-</md-menu>
-
-
-
-
-
-
+  <section class="text-toolbar  md-mini">
+    <md-button class="md-fab md-clean  md-mini">
+      <md-icon>text_format</md-icon>
+      <md-tooltip md-direction="top">Choose text format</md-tooltip>
+    </md-button>
+  
+    <md-button @click="changeAlign('right')" class="md-fab md-clean md-mini">
+      <md-icon>format_align_right</md-icon>
+      <md-tooltip md-direction="top">Text align right</md-tooltip>
+    </md-button>
+  
+    <md-button @click="changeAlign('center')" class="md-fab md-clean  md-mini">
+      <md-icon>format_align_center</md-icon>
+      <md-tooltip md-direction="top">Text align center</md-tooltip>
+    </md-button>
+  
+    <md-button @click="changeAlign('left')" class="md-fab md-clean  md-mini">
+      <md-icon>format_align_left</md-icon>
+      <md-tooltip md-direction="top">Text align left</md-tooltip>
+    </md-button>
+  
+    <md-button @click="changeBold" class="md-fab md-clean  md-mini">
+      <md-icon>format_bold</md-icon>
+      <md-tooltip md-direction="top">Bold text</md-tooltip>
+    </md-button>
+  
+    <md-button md-menu-trigger class="md-fab md-clean  md-mini color-picker-btn">
+      <md-icon>format_color_fill</md-icon>
+      <color-picker :change="updateColor" @changeColor="changeFontColor"></color-picker>
+      <md-tooltip md-direction="top">Change color of the text</md-tooltip>
+    </md-button>
+  
+    <md-menu md-align-trigger>
+      <md-button md-menu-trigger class="md-fab md-clean  md-mini">
+        <md-icon>format_size</md-icon>
+        <md-tooltip md-direction="top">Change text size</md-tooltip>
+      </md-button>
+      <md-menu-content>
+        <md-menu-item>12px</md-menu-item>
+        <md-menu-item>16px</md-menu-item>
+        <md-menu-item>18px</md-menu-item>
+        <md-menu-item>20px</md-menu-item>
+        <md-menu-item>22px</md-menu-item>
+        <md-menu-item>24px</md-menu-item>
+      </md-menu-content>
+    </md-menu>
+  
+    <!--<color-picker :change="updateColor"></color-picker>-->
+  
   </section>
 </template>
 
 <script>
+import ColorPicker from './ColorPicker'
+
 export default {
   name: 'ToolBar',
-  props:['cmp'],
-  created(){
-       this.cmpStyleEdit = JSON.parse(JSON.stringify(this.cmp));
+  props: ['cmp'],
+  components: { 'color-picker': ColorPicker },
+  created() {
+    this.cmpStyleEdit = JSON.parse(JSON.stringify(this.cmp));
   },
-  data () {
+  data() {
     return {
-        cmpStyleEdit: null,
-        
+      cmpStyleEdit: null,
+      color: ""
+
     }
   },
-  methods:{
-    changeAlign(dir){
+  methods: {
+    changeAlign(dir) {
       this.cmpStyleEdit.style.textAlign = dir;
       this.$emit('updateStyle', this.cmpStyleEdit)
-      
+
     },
-    changeBold(){
-      if(this.cmpStyleEdit.style.fontWeight==='normal')
-      this.cmpStyleEdit.style.fontWeight = 'bold';
-      else{
+    changeBold() {
+      if (this.cmpStyleEdit.style.fontWeight === 'normal')
+        this.cmpStyleEdit.style.fontWeight = 'bold';
+      else {
         this.cmpStyleEdit.style.fontWeight = 'normal';
       }
       this.$emit('updateStyle', this.cmpStyleEdit)
     },
+    updateColor: function (event) {
+      this.color = event.color;
+    },
+
+    changeFontColor(newColor) {
+      this.cmpStyleEdit.style.color = newColor;
+      this.$emit('updateStyle', this.cmpStyleEdit)
+
+    }
+
 
   }
 }
@@ -101,28 +99,33 @@ export default {
 
 
 <style scoped>
-.text-toolbar{
+.text-toolbar {
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
   width: 100%;
-
 }
-.c1{
+
+.c1 {
   background-color: white;
   width: 100%;
   text-align: center;
 }
-.c2{
+
+.c2 {
   background-color: red;
   width: 100%;
   text-align: center;
 }
-.c3{
+
+.c3 {
   background-color: green;
   width: 100%;
   text-align: center;
   font-weight: normal
 }
 
+.color-picker-btn {
+  overflow: initial;
+}
 </style>
