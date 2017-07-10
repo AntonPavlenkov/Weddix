482996f362d84afa456fadeb7897279f11365392
kerendot <kerendot@gmail.com>
2017-07-10 17:27:38 +0300

    merged

--- src/pages/PreviewPage.vue
+++ src/pages/PreviewPage.vue
@@ -22,6 +22,7 @@ import DumbSimpleText from '../components/cmpPublish/DumbSimpleText'
 import DumbSimpleTitle from '../components/cmpPublish/DumbSimpleTitle'
 import DumbCountDown from '../components/cmpPublish/DumbCountDown'
 import DumbImgTitle from '../components/cmpPublish/DumbImgTitle'
+import DumbGiftPicker from '../components/cmpPublish/DumbGiftPicker'
 export default {
   name: 'PublishPage',
   components: {
@@ -32,6 +33,7 @@ export default {
     DumbLocationMap,
     DumbSimpleText,
     DumbSimpleTitle,
+    DumbGiftPicker,
     Draggable
   },
   data() {
