2d722b8b781e356fb2a5a3d9a773546e29be6506
kerendot <kerendot@gmail.com>
2017-07-12 17:21:52 +0300

    moved server file

--- node_server/server.js
+++ node_server/server.js
@@ -1,34 +1,265 @@
 "use strict";
-var nodeMajor = +process.versions.node.split('.')[0];
-if (nodeMajor < 4) {
-	console.error('node < 4  is not supported!');
-	process.exit(1);
-} else {
-	if (nodeMajor < 6) {
-		console.info('Loading Babel transpiler for Oldish node!');
-		require('babel-register')();
-	}
-}
-var prompt = require('prompt');
-console.info('Welcome to Misterbit Simple REST Server!');
-// console.info('Notice: Only Mongo based server also has websocket support');
-var schema = {
-	properties: {
-		engine: {
-			pattern: /m|f/,
-			description: 'Which storage should I use mongodb (m) or file system (f)?',
-			message: 'Must select an engine!',
-			required: true
-		}
-	}
+
+var cl = console.log;
+
+const express = require('express'),
+	bodyParser = require('body-parser'),
+	cors = require('cors'),
+	mongodb = require('mongodb')
+
+const clientSessions = require("client-sessions");
+// const upload = require('./uploads');
+const app = express();
+
+var port = process.env.PORT || 3003;
+
+
+var corsOptions = {
+	origin: /.*/,
+	credentials: true
 };
-prompt.start();
-prompt.get(schema, function (err, result) {
-	switch (result.engine) {
-		case "m":
-			require('./server-full');
-			break;
-		case "f":
-			require('./server-load-from-files');
-	}
+
+// const serverRoot = 'http://localhost:3003/';
+// const baseUrl = serverRoot + 'data';
+
+// app.use(express.static('uploads'));
+
+app.use('/', express.static(__dirname));
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+// app.use(clientSessions({
+// 	cookieName: 'session',
+// 	secret: 'C0d1ng 1s fun 1f y0u kn0w h0w', // set this to a long random string!
+// 	duration: 30 * 60 * 1000,
+// 	activeDuration: 5 * 60 * 1000,
+// }));
+
+const http = require('http').Server(app);
+// const io = require('socket.io')(http);
+
+// var prm = dbConnect()
+// prm.then((res)=>{cl(res)}).catch((err)=>{cl(err)})
+
+// app.use('/',express.static)
+
+function dbConnect() {
+
+	return new Promise((resolve, reject) => {
+		// Connection URL
+		var url = 'mongodb://kerendot:weddix123@ds159591.mlab.com:59591/weddix';
+		// var url = 'mongodb://localhost:27017/weddix';
+		// Use connect method to connect to the Server
+		mongodb.MongoClient.connect(url, function (err, db) {
+			if (err) {
+				cl('Cannot connect to DB', err)
+				reject(err);
+			}
+			else {
+				cl("Connected to DB");
+				resolve(db);
+			}
+		});
+	});
+}
+
+// GETs a list
+app.get('/data/:objType', function (req, res) {
+	const objType = req.params.objType;
+	dbConnect().then(db => {
+		const collection = db.collection(objType);
+
+		collection.find({}).toArray((err, objs) => {
+			if (err) {
+				cl('Cannot get you a list of ', err)
+				res.json(404, { error: 'not found' })
+			} else {
+				cl("Returning list of " + objs.length + " " + objType + "s");
+				res.json(objs);
+			}
+			db.close();
+		});
+	});
 });
+
+// GETs a single
+app.get('/data/:objType/:id', function (req, res) {
+	cl('inside GET')
+	const objType = req.params.objType;
+	const objId = req.params.id;
+	cl(`Getting you an ${objType} with id: ${objId}`);
+	dbConnect()
+		.then((db) => {
+			const collection = db.collection(objType);
+			let _id;
+			try {
+				_id = new mongodb.ObjectID(objId);
+			}
+			catch (e) {
+				return Promise.reject(e);
+			}
+			return collection.findOne({ _id: _id })
+				.then((obj) => {
+					cl("Returning a single" + objType);
+					res.json(obj);
+					db.close();	
+				})
+				.catch(err => {
+					cl('Cannot get you that ', err)
+					res.json(404, { error: 'not found' })
+					db.close();	
+				})
+
+		});
+});
+
+// DELETE
+app.delete('/data/:objType/:id', function (req, res) {
+	const objType 	= req.params.objType;
+	const objId 	= req.params.id;
+	cl(`Requested to DELETE the ${objType} with id: ${objId}`);
+	dbConnect().then((db) => {
+		const collection = db.collection(objType);
+		collection.deleteOne({ _id: new mongodb.ObjectID(objId) }, (err, result) => {
+			if (err) {
+				cl('Cannot Delete', err)
+				res.json(500, { error: 'Delete failed' })
+			} else {
+				cl("Deleted", result);
+				res.json({});
+			}
+			db.close();
+		});
+
+	});
+
+
+});
+
+// POST - adds 
+app.post('/data/:objType', function (req, res) {
+	//console.log('req.file', req.file);
+	// console.log('req.body', req.body);
+
+	const objType = req.params.objType;
+	cl("POST for " + objType);
+
+	const obj = req.body;
+	delete obj._id;
+	// If there is a file upload, add the url to the obj
+	// if (req.file) {
+	// 	obj.imgUrl = serverRoot + req.file.filename;
+	// }
+
+	dbConnect().then((db) => {
+		const collection = db.collection(objType);
+
+		collection.insert(obj, (err, result) => {
+			if (err) {
+				cl(`Couldnt insert a new ${objType}`, err)
+				res.json(500, { error: 'Failed to add' })
+			} else {
+				cl(objType + " added");
+				res.json(obj);
+			}
+			db.close();
+		});
+	});
+
+});
+
+// PUT - updates
+app.put('/data/:objType/:id', function (req, res) {
+	const objType 	= req.params.objType;
+	const objId 	= req.params.id;
+	const newObj 	= req.body;
+	if (newObj._id && typeof newObj._id === 'string') newObj._id = new mongodb.ObjectID(newObj._id);
+
+	cl(`Requested to UPDATE the ${objType} with id: ${objId}`);
+	dbConnect().then((db) => {
+		const collection = db.collection(objType);
+		collection.updateOne({ _id: new mongodb.ObjectID(objId) }, newObj,
+			(err, result) => {
+				if (err) {
+					cl('Cannot Update', err)
+					res.json(500, { error: 'Update failed' })
+				} else {
+					res.json(newObj);
+				}
+				db.close();
+			});
+	});
+});
+
+// Basic Login/Logout/Protected assets
+// app.post('/login', function (req, res) {
+// 	dbConnect().then((db) => {
+// 		db.collection('user').findOne({ username: req.body.username, pass: req.body.pass }, function (err, user) {
+// 			if (user) {
+// 				cl('Login Succesful');
+// 				delete user.pass;
+// 				req.session.user = user;  //refresh the session value
+// 				res.json({ token: 'Beareloginr: puk115th@b@5t', user });
+// 			} else {
+// 				cl('Login NOT Succesful');
+// 				req.session.user = null;
+// 				res.json(403, { error: 'Login failed' })
+// 			}
+// 		});
+// 	});
+// });
+
+// app.get('/logout', function (req, res) {
+// 	req.session.reset();
+// 	res.end('Loggedout');
+// });
+
+// function requireLogin(req, res, next) {
+// 	if (!req.session.user) {
+// 		cl('Login Required');
+// 		res.json(403, { error: 'Please Login' })
+// 	} else {
+// 		next();
+// 	}
+// };
+// app.get('/protected', requireLogin, function (req, res) {
+// 	res.end('User is loggedin, return some data');
+// });
+
+app.use('/*', express.static(__dirname));
+
+// Kickup our server 
+// Note: app.listen will not work with cors and the socket
+app.listen(port, function () {
+// http.listen(3003, function () {
+	console.log('server started ' + port);
+});
+
+
+// io.on('connection', function (socket) {
+// 	console.log('a user connected');
+// 	socket.on('disconnect', function () {
+// 		console.log('user disconnected');
+// 	});
+// 	socket.on('chat message', function (msg) {
+// 		// console.log('message: ' + msg);
+// 		io.emit('chat message', msg);
+// 	});
+// });
+
+// cl('WebSocket is Ready');
+
+// Some small time utility functions
+
+
+
+
+// function cl(...params) {
+// 	console.log.apply(console, params);
+// }
+
+// Just for basic testing the socket
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/test-socket.html');
+// });
